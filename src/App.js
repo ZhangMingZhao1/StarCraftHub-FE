9552db2ea467a9a84a431cbd470b641b694d1c8b
张明朝 <1104272319@qq.com>
2018-06-02 18:42:24 +0800

    重构header组件

--- src/App.js
+++ src/App.js
@@ -3,52 +3,19 @@ import './App.css';
 import {Button} from 'antd';
 import { Menu} from 'antd';
 import {Router,Route,Link} from 'react-router';
+import CMenu from './components/menu/index';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class App extends Component {
   constructor() {
     super();
-    this.handleClick = this.handleClick.bind(this);
-  }
-  state = {
-    current: 'video'
-  }
-  handleClick = (e) => {
-    console.log('click ', e);
-    this.setState({
-      current: e.key,
-    });
-  }
+  } 
   render() {
     return (
       <div className="App">
-        <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-      >
-        <Menu.Item key="introduce">
-          {/* <Icon type="mail" />Navigation One */}
-          走进星际
-        </Menu.Item>
-        <Menu.Item key="video">
-          视频
-        </Menu.Item>
-        <Menu.Item key="picture">
-          图片
-        </Menu.Item>
-        <Menu.Item key="news">
-          新闻
-        </Menu.Item>
-        <Menu.Item key="discuss">
-          论坛
-        </Menu.Item>
-        <Menu.Item key="data">
-          一周热数据
-        </Menu.Item>
-      </Menu>
-      </div>
+        <CMenu></CMenu>
+      </div> 
     );
   }
 }
