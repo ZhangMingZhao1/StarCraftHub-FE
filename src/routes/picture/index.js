3e62c13cb5d15cf74307e9f95ee942845c09e500
张明朝 <1104272319@qq.com>
2018-07-09 22:46:35 +0800

    图片加载由七牛云上OSS资源请求动态加载

--- src/routes/picture/index.js
+++ src/routes/picture/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './index.less';
 import {Button, Upload, Icon, message} from 'antd';
+import 'whatwg-fetch';
 
 class Picture extends Component {
 	constructor() {
@@ -29,13 +30,21 @@ class Picture extends Component {
 		console.log(e.target.src);
 		let picsrc = e.target.src;
 		this.setState({picsrc:picsrc,display:"block"});
-
 	}
 
+	componentWillMount() {
+		var myFetchOptions = {
+			method: 'GET',
+			mode: 'cors'
+		};
+		fetch("http://112.74.191.12:3000/getat",myFetchOptions).then(res => res.json()).then(json => {this.setState({pictureList:json});console.log(json);});
+		
+	}
 	popupClick() {
 		this.setState({picsrc:"",display:"none"});
 	}
 	render() {
+		let imgDom;
 		const props = {
 			action: '/upload',
 			onChange({ file, fileList }) {
@@ -52,15 +61,20 @@ class Picture extends Component {
 			},
 			defaultFileList: [],
 		  };
-
+			imgDom = this.state.pictureList.map( (v,k) => {
+				return <div><img src={`http://pao1opmq0.bkt.clouddn.com/${v.key}`} alt=""/></div> })
+				
+	
 		return (
 		<div>
 			<div className="container" onClick={this.hangleOnClick}>
-				<div><img src="http://pao1opmq0.bkt.clouddn.com/sc2tt.jpg" alt=""/></div>
+				
+				{/* <div><img src="http://pao1opmq0.bkt.clouddn.com/sc2tt.jpg" alt=""/></div>
 				<div><img src="http://pao1opmq0.bkt.clouddn.com/scp.jpg" alt=""/></div>
 				<div><img src="http://pao1opmq0.bkt.clouddn.com/sc2pp.jpg" alt=""/></div>
 				<div><img src="http://pao1opmq0.bkt.clouddn.com/sc2z.jpg" alt=""/></div>
-				<div><img src="http://pao1opmq0.bkt.clouddn.com/sc2t.jpg" alt=""/></div>
+				<div><img src="http://pao1opmq0.bkt.clouddn.com/sc2t.jpg" alt=""/></div> */}
+				{imgDom}
 			</div>
 			<div className="popup" style={{display: `${this.state.display}`}} onClick={this.popupClick} >
     		<div className="bg"><img src={this.state.picsrc} alt=""/></div>
